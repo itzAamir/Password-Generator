eb500e5a95faaeb3ba831b218f52f43c42f37afb
itzAamir <amirpc190320@gmail.com>
2020-11-08 12:59:49 +0530

    Initial Commit

---
+++ main.js
@@ -0,0 +1,20 @@
+const submit = document.getElementById("submit");
+let input = document.getElementById("password");
+submit.addEventListener("click", ()=>{
+    let inputValue = generate(16);
+    input.value = inputValue;
+})
+
+const charset = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","y","z","1","2","3","4","5","6","7","8","9","0","!","@","#","$","%","^","&","*","(",")",",",".","<",">","/","?","-","="];
+
+function generate(length = 10) {
+    let password;
+    for(let i=0; i < length; i++){
+        let char = charset[Math.floor(Math.random()* charset.length)]
+        password += char;
+    }
+    if (password.includes("undefined")){
+        newPass = password.replace("undefined", "")
+    }   
+    return newPass
+}
